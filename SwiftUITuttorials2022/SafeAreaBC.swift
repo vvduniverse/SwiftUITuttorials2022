eb4912643d8453f52bf72a03d39c67bd04143c22
Vahtee Boo <Vahtee.Boo@gmail.com>
2022-02-18 20:51:00 +0200

    part17

---
+++ SwiftUITuttorials2022/SafeAreaBC.swift
@@ -0,0 +1,58 @@
+//
+//  SafeAreaBC.swift
+//  SwiftUITuttorials2022
+//
+//  Created by Vahtee Boo on 18.02.2022.
+//
+
+import SwiftUI
+
+struct SafeAreaBC: View {
+    var body: some View {
+        
+        ScrollView {
+            VStack {
+                Text("Title goes here")
+                    .font(.largeTitle)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                ForEach(0..<10) { index in
+                    RoundedRectangle(cornerRadius: 25)
+                        .fill(Color.white)
+                        .frame(height: 150)
+                        .shadow(radius: 10)
+                        .padding(15)
+                }
+            }
+        }
+        .background(
+            Color.indigo
+//                .ignoresSafeArea()
+        )
+        
+        
+//        ZStack {
+//            //background
+//            Color.indigo
+//                .ignoresSafeArea()
+//
+//            //foreground
+//            Color.green
+//
+//            VStack {
+//                Text("Hello, World!")
+//                Spacer()
+//            }
+////            .frame(maxWidth: .infinity)
+////            .background(Color.green)
+////            .background(ignoresSafeAreaEdges: .all)
+//
+//        }
+    }
+}
+
+struct SafeAreaBC_Previews: PreviewProvider {
+    static var previews: some View {
+        SafeAreaBC()
+    }
+}
