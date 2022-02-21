3a89b8e7e1e4e37343b209200313dc6f7bcc633a
Vahtee Boo <Vahtee.Boo@gmail.com>
2022-02-21 16:17:50 +0200

    part30

---
+++ SwiftUITuttorials2022/NavigationViewBC.swift
@@ -0,0 +1,122 @@
+//
+//  NavigationViewBC.swift
+//  SwiftUITuttorials2022
+//
+//  Created by Vahtee Boo on 21.02.2022.
+//
+
+import SwiftUI
+
+struct NavigationViewBC: View {
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                NavigationLink("hello, banana") {
+                    SecondScreen()
+                }
+                Text("Hello, World!")
+                Text("Hello, World!")
+                Text("Hello, World!")
+                Text("Hello, World!")
+                Text("Hello, World!")
+                
+            }
+            //                .navigationBarTitleDisplayMode(.inline)
+            .navigationTitle("Bunch of strings")
+            //            .navigationBarHidden(true)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    NavigationLink(destination: SecondScreen()) {
+                        Image(systemName: "1.square")
+                    }
+                }
+                
+                ToolbarItem(placement: .navigationBarLeading) {
+                    NavigationLink(destination: ThirdScreen()) {
+                        Image(systemName: "2.square")
+                    }
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Image(systemName: "gear")
+                        .foregroundColor(.orange)
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Image(systemName: "flame")
+                        .foregroundColor(.green)
+                }
+                
+                
+                ToolbarItem(placement: .bottomBar) {
+                    NavigationLink(destination: SecondScreen()) {
+                        Image(systemName: "1.square")
+                    }
+                }
+                
+                ToolbarItem(placement: .bottomBar) {
+                    NavigationLink(destination: ThirdScreen()) {
+                        Image(systemName: "2.square")
+                    }
+                }
+                
+                ToolbarItem(placement: .bottomBar) {
+                  Spacer()
+                }
+                
+                ToolbarItem(placement: .bottomBar) {
+                    Image(systemName: "gear")
+                        .foregroundColor(.orange)
+                }
+                
+                ToolbarItem(placement: .bottomBar) {
+                    Image(systemName: "flame")
+                        .foregroundColor(.green)
+                }
+            }
+        }
+    }
+}
+
+struct SecondScreen: View {
+    var body: some View {
+        ZStack {
+            Color.green.ignoresSafeArea()
+            
+            NavigationLink("hello, indigo") {
+                ThirdScreen()
+            }
+            .navigationTitle("Second screen")
+        }
+    }
+}
+
+struct ThirdScreen: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        ZStack {
+            Color.indigo.ignoresSafeArea()
+            
+            VStack {
+                Button {
+                    presentationMode.wrappedValue.dismiss()
+                } label: {
+                    Text("Close thid sheet")
+                }
+                Text("The END")
+                    .navigationTitle("Third Screen")
+                    .navigationBarHidden(true)
+            }
+            
+        }
+    }
+}
+
+struct NavigationViewBC_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationViewBC()
+        //        SecondScreen()
+    }
+}
