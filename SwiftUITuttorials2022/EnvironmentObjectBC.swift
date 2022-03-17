74a1f2739fe84c7406ab5147dc7bb58f3036045e
Vahtee Boo <Vahtee.Boo@gmail.com>
2022-03-17 19:03:27 +0200

    part51

---
+++ SwiftUITuttorials2022/EnvironmentObjectBC.swift
@@ -0,0 +1,95 @@
+//
+//  EnvironmentObjectBC.swift
+//  SwiftUITuttorials2022
+//
+//  Created by Vahtee Boo on 17.03.2022.
+//
+
+import SwiftUI
+
+class EnvironmentViewModel: ObservableObject {
+    
+    @Published var dataArray: [String] = []
+    
+    init() {
+        getData()
+    }
+    
+    func getData() {
+        self.dataArray.append(contentsOf: ["AirTag", "AirPods", "Mac mini", "iPhone", "iPad", "iMac", "Apple Watch", "Mac Studio", "MacBook"])
+    }
+}
+
+struct EnvironmentObjectBC: View {
+    
+    @StateObject var viewModel = EnvironmentViewModel()
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(viewModel.dataArray, id: \.self) { item in
+                    NavigationLink {
+                        DetailView(selectedItem: item)
+                    } label: {
+                        Text(item)
+                    }
+
+                }
+            }
+            .navigationTitle("Apple Devices")
+        }
+        .environmentObject(viewModel)
+    }
+}
+
+struct DetailView: View {
+    
+    let selectedItem: String
+    
+    var body: some View {
+        ZStack {
+            Color.orange.ignoresSafeArea()
+            
+            NavigationLink(destination: FinalView()) {
+                Text(selectedItem)
+                    .font(.headline)
+                    .foregroundColor(.orange)
+                    .padding()
+                    .padding(.horizontal)
+                    .background(Color.white)
+                    .cornerRadius(30)
+            }
+            
+        }
+    }
+}
+
+struct FinalView: View {
+    
+    @EnvironmentObject var viewModel: EnvironmentViewModel
+    
+    var body: some View {
+        ZStack {
+            LinearGradient(colors:  [.mint, .blue.opacity(0.8), .black.opacity(0.9)], startPoint: .topLeading, endPoint: .bottomTrailing)
+                .ignoresSafeArea()
+            
+            ScrollView {
+                VStack(spacing: 20) {
+                    ForEach(viewModel.dataArray, id: \.self) { item in
+                        Text(item)
+                    }
+                }
+                .foregroundColor(.white)
+                .font(.largeTitle)
+            }
+        }
+    }
+}
+
+struct EnvironmentObjectBC_Previews: PreviewProvider {
+    static var previews: some View {
+        EnvironmentObjectBC()
+//        DetailView(selectedItem: "iPhone")
+//        FinalView()
+    }
+}
